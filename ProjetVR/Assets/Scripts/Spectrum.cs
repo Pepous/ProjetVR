37469c3d83e9963021c32527f2351a6f311f86ca
Pri-cell <66022612+Pri-cell@users.noreply.github.com>
2022-04-06 09:25:04 -0400

    ajout moving cube

--- ProjetVR/Assets/Scripts/Spectrum.cs
+++ ProjetVR/Assets/Scripts/Spectrum.cs
@@ -13,21 +13,12 @@ public class Spectrum : MonoBehaviour
     void Start()
     {
         
-        float x = -512, y = 0, z = 0;
-        // calculate
-        float xIncrement = the_pfCube.transform.localScale.x * 2;
 
         for (int i = 0; i < the_cubes.Length; i++)
         {
             
             // instantiate prefab game object
             GameObject go = Instantiate(the_pfCube);
-            // color material
-            go.GetComponent<Renderer>().material.SetColor("_BaseColor", new Color(.5f, 1, .5f));
-            // default position
-            go.transform.position = new Vector3(x, y, z);
-            // increment the x position
-            x += xIncrement;
             // scale it to be 2x wider
             go.transform.localScale = new Vector3(2, 0, 1);
             // give a name
@@ -38,8 +29,6 @@ public class Spectrum : MonoBehaviour
             the_cubes[i] = go;
 
         }
-        //position this
-        this.transform.position = new Vector3(this.transform.position.x, -100, this.transform.position.z);
 
     }
 
@@ -51,15 +40,11 @@ public class Spectrum : MonoBehaviour
 
         for(int i = 0; i < the_cubes.Length; i++)
         {
-            float y = 600 * Mathf.Sqrt(spectrum[i]);
+            float spct = 600 * Mathf.Sqrt(spectrum[i]);
             the_cubes[i].transform.localScale =
-                new Vector3(the_cubes[i].transform.localScale.x,
-                y,
-                the_cubes[i].transform.localScale.z);
-            the_cubes[i].transform.localPosition =
-                new Vector3(the_cubes[i].transform.localPosition.x,
-                y/2,
-                the_cubes[i].transform.localPosition.y);
+                new Vector3(spct,
+                spct,
+                spct);
         }
     }
 }
